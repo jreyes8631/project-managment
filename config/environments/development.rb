27f88d0506fd3b742efa38c17ecf2186ff0a48d7
jreyes8631 <jreyes8631@gmail.com>
2021-07-25 13:59:43 -0400

    add local host on 3001

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
