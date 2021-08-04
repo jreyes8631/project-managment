1b11ef5b29af993bb18dd3259b7889964a5ce542
jreyes8631 <jreyes8631@gmail.com>
2021-08-04 15:48:39 -0400

    add status option to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   belongs_to :project
 
   validates :status, inclusion: { in: ['not-started', 'in-progress', 'completed']}
+
+  STATUS_OPTIONS = [
+    ['Not Started', 'not started'],
+    ['In porgress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
 end
